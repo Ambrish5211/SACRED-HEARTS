41dd43f7c189cf92c4972ce76b731f966af11051
Ambrish Kumar <ambrish19.5211@gmail.com>
2023-12-30 17:31:06 +0530

    frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,7 @@
+import "./App.css";
+
+function App() {
+  return <div className="bg-gray-800 3xl border">Hello world</div>;
+}
+
+export default App;
---
+++ frontend/src/main.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Toaster } from "react-hot-toast";
+
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store.js";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+      <Toaster />
+    </BrowserRouter>
+  </Provider>
+);

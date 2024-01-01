0c7b5eb841349e3a42016a853d8658249a8af0e8
Ambrish Kumar <ambrish19.5211@gmail.com>
2024-01-01 20:12:38 +0530

    movies card implemented

---
+++ frontend/src/Pages/Movies/MovieDescription.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function MovieDescription() {
+  return <div>MovieDescription</div>;
+}
--- frontend/src/Pages/Movies/MoviesList.jsx
+++ frontend/src/Pages/Movies/MoviesList.jsx
@@ -1,17 +1,16 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
-import CourseCard from "../../Components/CourseCard";
+// import CourseCard from "../../Components/CourseCard";
 import HomeLayout from "../../Layouts/HomeLayout";
-import { getAllCourses } from "../../redux/slices/courseSlice";
-
-function CourseList() {
+import { getAllMovies } from "../../redux/slices/movieSlice";
+function MoviesList() {
   const dispatch = useDispatch();
 
-  const { courseList } = useSelector((state) => state.course);
+  const { movieList } = useSelector((state) => state.movie);
 
-  async function loadCourses() {
-    await dispatch(getAllCourses());
+  async function loadMovies() {
+    await dispatch(getAllMovies());
   }
 
   useEffect(() => {
@@ -26,7 +25,7 @@ function CourseList() {
           <span className="font-bold text-yellow-500">Industry experts</span>
         </h1>
         <div className="mb-10 flex flex-wrap gap-16 justify-between px-12">
-          {courseList?.map((element) => {
+          {movieList?.map((element) => {
             return <CourseCard key={element._id} data={element} />;
           })}
         </div>
@@ -35,4 +34,4 @@ function CourseList() {
   );
 }
 
-export default CourseList;
+export default MoviesList;

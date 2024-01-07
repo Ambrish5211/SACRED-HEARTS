4dce41d847db41c4a15c3d61031a92e7c8355aff
Ambrish Kumar <ambrish19.5211@gmail.com>
2024-01-07 22:51:15 +0530

    settings updated

--- backend/Movie_Downloader_Backend/settings.py
+++ backend/Movie_Downloader_Backend/settings.py
@@ -121,8 +121,8 @@ DATABASES = {
     }
 }
 
-# database_url = os.environ.get("DATABASE_URL")
-# DATABASES['default'] = dj_database_url.parse(database_url)
+database_url = os.environ.get("DATABASE_URL")
+DATABASES['default'] = dj_database_url.parse(database_url)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators

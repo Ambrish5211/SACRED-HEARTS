0d42b3c77dd64a03fdaa7960393a15152fd00147
Ambrish <ambrish19.5211@gmail.com>
2024-09-13 02:08:17 +0530

    async handler layer

---
+++ backend/src/utils/asyncHandler.js
@@ -0,0 +1,17 @@
+// const asyncHandler = (requestHandler) => {
+//     return (req, res, next) => {
+//       Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+//     };
+//   };
+
+  const asyncHandler = (requestHandler) => async (req, res, next) => {
+      try {
+        await requestHandler(req, res, next);
+      } catch (error) {
+        res.staus(error.code || 500).json({
+          success: false,
+          message: error.message,
+        });
+      }
+    };
+    

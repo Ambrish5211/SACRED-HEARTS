adda4a2d85dcfa6df6e5a4fcad7772147a0cf04d
Ambrish <ambrish19.5211@gmail.com>
2025-02-17 01:31:20 +0530

    build fix 3

--- frontend/src/config/axiosInstance.js
+++ frontend/src/config/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://sacred-hearts.onrender.com/";
+const BASE_URL = "https://sacred-hearts.onrender.com/api/v1";
 const axiosInstance = axios.create();
 
 axiosInstance.defaults.baseURL = BASE_URL;

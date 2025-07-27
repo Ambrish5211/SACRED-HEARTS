f484d3fed18a044b7be7af350cc27fcf8c15f917
Ambrish Kumar <ambrish19.5211@gmail.com>
2025-07-28 00:53:05 +0530

    update docker config

--- frontend/src/config/axiosInstance.js
+++ frontend/src/config/axiosInstance.js
@@ -1,14 +1,7 @@
 import axios from "axios";
 
-
-
-
 const BASE_URL = import.meta.env.VITE_API_BASE_URL
 
-
-
-
-console.log("BASE_URL", BASE_URL);
 const axiosInstance = axios.create();
 
 axiosInstance.defaults.baseURL = BASE_URL;
